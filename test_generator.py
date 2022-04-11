97ab901501fd90774f41ae527d4e1478c24dbd95
oneku16 <eku.ulanov@yandex.com>
2022-04-11 16:57:17 +0600

    updated test_gen

--- test_generator.py
+++ test_generator.py
@@ -1,46 +1,61 @@
 import sympy as sp
 import numpy as np
 from random import randint as rd
+x = sp.Symbol('x')
 
 
 
-#  class since we are going to generate problems for different topics
-class TestGenerator:
 
 
-    def __init__(self, topic: str):
+
+
+
+
+
+
+def Diffs():
+
+    pass 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def linear_equation():
+
+#     operation = ['-', '+', '*', '/']
+#     equation = ""
+
+#     length = rd(3,5)
+#     used_x = True
+#     for i in range(length):
         
-        self.topic = topic # topic: (linear eq, defined integral and etc)
-        self.x = sp.Symbol('x')
-        self.equation = ""
+#         if i < (length - 1): 
+#             equation += str(rd(2,10)) + operation[rd(0,3)]
 
-    #  depending on the topic this func will run the function that we need
-    def __generate(self):
+#             if len(equation) >= 3 and used_x:
+#                 equation += 'x*'
+#                 used_x = False
 
-        pass 
-    # it is function for linear eq. just demo
-    def linear_equation(self):
-
-        operation = ['-', '+', '*', '/']
-
-        length = rd(3,5)
-        used_x = True
-        for i in range(length):
-            
-            if i < (length - 1): 
-                self.equation += str(rd(2,10)) + operation[rd(0,3)]
-
-                if len(self.equation) >= 3 and used_x:
-                    self.equation += 'x'
-                    used_x = False
-
-            else: self.equation += str(rd(2,10))
-        
-        return "function itself: ", self.equation, " simplified: ", sp.simplify(self.equation, doit = False)
-
-
-t = TestGenerator('l').linear_equation() # just to run this code! it will be removed from final version of code.
-print(t)
+#         else: equation += str(rd(2,10))
+    
+#     return "function itself: ", equation, " simplified: ", sp.simplify(equation, doit = False)
 
 
 

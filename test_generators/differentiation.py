19932d34487e94bcf5cb46e3296aedce5b5b7248
baistan.tashkuov <baistan.xi@gmail.com>
2022-04-13 15:34:40 +0600

    Differentiation generator was updated

---
+++ test_generators/differentiation.py
@@ -0,0 +1,47 @@
+import random
+from sympy import simplify
+
+
+def genDifferentiationProblem(diff_lvl=2):
+
+    # dependencies
+    problem = ''
+    types = {
+        'Logarithmic': ['ln'],
+        'Trigonometric': ['sin', 'cos', 'tan', 'cot', 'sec'],
+        'Exponential': ['exp']
+    }
+    power = random.randint(2, 5)
+
+    if diff_lvl == 1:
+        coeff1 = random.randint(2, 10)
+        problem += "{}*x^{}".format(coeff1, power)
+
+    if diff_lvl == 2:
+        coeff1 = random.randint(2, 10)
+        coeff2 = random.randint(2, 10)
+        coeff3 = random.randint(2, 10)
+        power2 = random.randint(2, 4)
+        power3 = random.randint(1, 3)
+
+        problem += "{}*x^{}+{}*x^{}+{}*x^{}".format(coeff1, power, coeff2, power2, coeff3, power3)
+
+    if diff_lvl == 3:
+        coeff1 = random.randint(1, 5)
+        func_type = random.choices(list(types.keys()), weights=(1, 4, 1))[0]
+        func = random.choice(types[func_type])
+        problem += "{}*{}(x)+{}".format(coeff1, func, genDifferentiationProblem(1))
+
+    if diff_lvl == 4:
+        func_type = random.choices(list(types.keys()), weights=(2, 4, 2))[0]
+        func = random.choice(types[func_type])
+        problem += "{}({})".format(func, genDifferentiationProblem(1))
+
+    if diff_lvl == 5:
+        operator = random.choice(('/', '*'))
+        problem = "({}){}({})".format(genDifferentiationProblem(2), operator, genDifferentiationProblem(3))
+
+    return simplify(problem)
+
+
+print(genDifferentiationProblem(5))

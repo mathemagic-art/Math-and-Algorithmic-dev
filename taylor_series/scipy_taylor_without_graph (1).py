f07dfe31b8462c86628d9b0a5f4ef6e47a1e5434
sharukhhhan <shodavlat21@gmail.com>
2022-04-14 20:39:30 +0600

    last edits

--- taylor_series/scipy_taylor_without_graph (1).py
+++ taylor_series/scipy_taylor_without_graph (1).py
@@ -1,7 +1,6 @@
 from sympy import lambdify, sympify, diff, E
 from math import factorial
 from sympy.abc import x
-import numpy as np
 
 
 def taylor(function, num_of_iter, center):
@@ -12,5 +11,3 @@ def taylor(function, num_of_iter, center):
       taylorPolynomial += '+' + f_diff +'/'+str(factorial(i))+'*(x-{})**{}'.format(center, i)
   taylorPolynomial = sympify(taylorPolynomial, rational=True)
   return str(taylorPolynomial)
-
-print(taylor('e**x', 5, 0))

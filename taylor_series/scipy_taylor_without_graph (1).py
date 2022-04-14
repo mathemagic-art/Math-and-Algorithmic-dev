ed125055759011092097ca96af8ab0ecc87ee453
sharukhhhan <shodavlat21@gmail.com>
2022-04-14 20:35:31 +0600

    changes the datatype of return

--- taylor_series/scipy_taylor_without_graph (1).py
+++ taylor_series/scipy_taylor_without_graph (1).py
@@ -5,9 +5,12 @@ import numpy as np
 
 
 def taylor(function, num_of_iter, center):
+  function = function.replace('e', 'E')
   taylorPolynomial = str(lambdify(x, sympify(function))(center))
   for i in range(1, num_of_iter):
       f_diff = str(lambdify(x, diff(function, x, i))(center))
       taylorPolynomial += '+' + f_diff +'/'+str(factorial(i))+'*(x-{})**{}'.format(center, i)
   taylorPolynomial = sympify(taylorPolynomial, rational=True)
-  return taylorPolynomial
+  return str(taylorPolynomial)
+
+print(taylor('e**x', 5, 0))

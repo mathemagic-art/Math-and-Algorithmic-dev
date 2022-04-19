1334a629387badb1c17c880099fe5bc2430f36b8
sharukhhhan <shodavlat21@gmail.com>
2022-04-19 10:59:19 +0600

    updated integ func (implemented parse and output func)

--- Integrating_Calculator/integration_calculator.py
+++ Integrating_Calculator/integration_calculator.py
@@ -1,27 +1,17 @@
-from sympy import sympify, integrate, lambdify, E
+from sympy import python, sympify, integrate, lambdify, E
 from sympy.abc import x
+import sys
+sys.path.append('parse_and_output')
+from parse_and_output import parseFunc, outputFunc
 
-def integration_calculator(function,variable,lower_bound='inf',upper_bound='inf'):
-  # By default the limits are set to infinity and it will calculate the indefinite integral,
-  # If the limits are given then it will return the definite integral
-  """
-    integrate(f, x) returns the indefinite integral 
-    integrate(f, (x, a, b)) returns the definite integral 
-  """
-  if lower_bound == upper_bound and upper_bound=='inf': # Checking the conidtion for indefinte integral
-    return integrate(function,variable) # given the indefinite integral
-  else: # otherwise it is a definite integral
-    return integrate(function, (variable, lower_bound, upper_bound)) # given the definite integral
+def indefinite_integration_calculator(function: str) -> str:
+  return outputFunc(integrate(parseFunc(function)))
 
 
-def definite_integral_calculator(function, lower_bound, upper_bound):
-  function = function.replace('e', 'E')
-  function = function.replace('ln', 'log')
+def definite_integration_calculator(function, lower_bound, upper_bound):
+  function = parseFunc(function)
   a = lambdify(x, integrate(sympify(function))) #integrating and lammbdifying a given function
-  return str(a(upper_bound)-a(lower_bound))
-
-print(definite_integral_calculator('ln(x)', 3, 6))
+  return outputFunc("{:.5f}".format((upper_bound)-a(lower_bound)))
 
 
-
-
+print(definite_integration_calculator('1/x', 3, 6))
